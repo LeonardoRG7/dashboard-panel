f49873a5dbd57aa2f22b272edb95986de58251e7
LeonardoRG7 <leonardoriascos14@gmail.com>
2024-03-08 12:41:16 -0500

    Login and Logout

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { DevelopersService } from 'src/app/core/services/developers.service';
 
 @Component({
@@ -13,7 +14,8 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private _developersService: DevelopersService,
-    private _router: Router
+    private _router: Router,
+    private _toastr: ToastrService
   ) {}
 
   ngOnInit(): void {
@@ -25,12 +27,17 @@ export class LoginComponent implements OnInit {
   login() {
     this._developersService.loginUser(this.username, this.password).subscribe(
       (response) => {
-        console.log('Login successful', response);
-        localStorage.setItem('token', response.token);
-        this._router.navigate(['/dashboard']);
+        if (response && response.length > 0) {
+          localStorage.setItem('token', JSON.stringify(response)); // Guardamos el token en el localStorage para mantener nuestra session
+          this._router.navigate(['/dashboard']);
+          this._toastr.success('Inicio de sesión exitoso', 'Éxito');
+        } else {
+          this._toastr.error('Credenciales inválidas', 'Error');
+        }
       },
       (error) => {
-        console.error('Login failed', error);
+        console.error('Error al iniciar sesión:', error);
+        this._toastr.error('Error al iniciar sesión', 'Error');
       }
     );
   }
--- src/app/modules/components/header/header.component.ts
+++ src/app/modules/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Notification } from 'src/app/core/interfaces/notification';
 import { DevelopersService } from 'src/app/core/services/developers.service';
 
@@ -9,11 +11,21 @@ import { DevelopersService } from 'src/app/core/services/developers.service';
 })
 export class HeaderComponent implements OnInit {
   notifications: Notification[] = [];
-  constructor(private _developersService: DevelopersService) {}
+  constructor(
+    private _developersService: DevelopersService,
+    private _toastr: ToastrService,
+    private _router: Router
+  ) {}
 
   ngOnInit(): void {
     this._developersService.getNotificaction().subscribe((res) => {
       this.notifications = res;
     });
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this._router.navigate(['/']);
+    this._toastr.success('Sesión cerrada correctamente', 'Éxito');
   }
 }
